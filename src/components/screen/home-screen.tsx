6ef92bfad0ef7b40b33baeaeb9cce35e0cdef35f
José Carlos Quichiz Santome <josec.quichiz@gmail.com>
2021-05-04 14:19:48 -0500

    Validations

--- src/components/screen/home-screen.tsx
+++ src/components/screen/home-screen.tsx
@@ -1,19 +1,58 @@
+import { useState } from 'react';
 import {
   useSelector
 } from 'react-redux'
+
+import {
+  isChecked,
+  isRequired,
+  isValidCellphone,
+  isValidDocument,
+  isValidName
+} from '../../utils/validate'
 
 import {Field, CheckboxField, SelectInput, TextInput, DateInput} from '../field'
 
 function HomeScreen (){
   const optionLists = useSelector(store => (store as any).personalInfo.extra);
 
-  
+  const [invalidFields, setInvalidFields] = useState({});
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const fd = new FormData(event.target)
+
+    const invalidFields: any = {};
+
+    const documentType: any = fd.get('documentType');
+
+    fd.forEach((value, key) => {
+      let validInput = true;
+
+      switch(key){
+        case 'documentType': validInput = isValidDocument(value, documentType); break;
+        case 'birthday': validInput = isRequired(value); break;
+        case 'cellphone': validInput = isValidCellphone(value); break;
+        case 'tyc': validInput = isChecked(value); break;
+      }
+
+      invalidFields[key] = validInput
+    });
+
+    setInvalidFields(invalidFields);
+    // var obj: any = {};
+    // fd.forEach(function(value, key){
+    //     obj[key] = value;
+    // });
+    // console.log( JSON.stringify(obj));
+  }
+
   return (
     <div>
       <h2 className="text--h2 text--light text--h2">Obtén tu <span className="text--regular text--blue">seguro ahora</span></h2>
       <p className="page__description text--light">Ingresa los datos para comenzar.</p>
 
-      <form action="#" className="form">
+      <form action="#" className="form" onSubmit={handleSubmit}>
 
         <Field
           input={ <TextInput placeholder="Nro. de Documento" name="documentNumber"/> }
---
+++ src/utils/validate.tsx
@@ -0,0 +1,23 @@
+export function isRequired(value: any): boolean{
+  return !!value;
+}
+
+export function isValidName(value: any): boolean{
+  return !/[^a-zA-ZáÁàÀâÂåÅãÃäÄæÆçÇéÉèÈêÊëËíÍìÌîÎïÏñÑóÓòÒôÔøØõÕöÖßúÚùÙûÛüÜÿ\s,.]/.test(value)
+}
+
+export function isValidCellphone(value: any): boolean{
+  return /^9[0-9]{8}$/.test(value);
+}
+
+export function isChecked(value: any): boolean{
+  return value === 'on';
+}
+
+export function isValidDocument(value: any, type: string): boolean{
+  switch(type){
+    case 'DNI': return /^[0-9]{8}$/.test(value);
+    case 'RUC': return /(10|20)[0-9]{9}/.test(value);
+    default: return false;
+  }
+}
